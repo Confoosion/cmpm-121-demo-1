2f315b102785578d773ff891f97cc6b68a55245e
Jake Sales <jfsales@ucsc.edu>
2024-10-08 17:04:56 -0700

    Step 1 Complete
    Added a button that functions

--- src/main.ts
+++ src/main.ts
@@ -8,3 +8,12 @@ document.title = gameName;
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+const button = document.createElement("button");
+button.innerHTML = "😎";
+
+button.addEventListener("click", () => {
+  alert("Button clicked!");
+});
+
+app.append(button);
